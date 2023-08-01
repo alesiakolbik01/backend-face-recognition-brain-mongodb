cdf463f831c6bae219bc7a9c89abf514b524b275
Alesia Kolbik <alesyakolbik@gmail.com>
2023-08-01 19:28:36 +0200

    first commit

---
+++ db-config.js
@@ -0,0 +1,8 @@
+export const dbConfig = {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'api-key': 'K5flkjOfBLRgo45Jlq8HVHCQ4Wwefc89AZi6h00NdztJSLrSveq2zpbtP5QAa8If',
+    }
+};
